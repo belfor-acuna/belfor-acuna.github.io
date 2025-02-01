6199bc11825771738884e77753f66206d1c3392f
Belfor Acuña <131080733+belfor-acuna@users.noreply.github.com>
2025-02-01 03:08:43 -0300

    feat: :sparkles: About finished, adding of new project components and start of first project description
    Finished "about me" site with what drives me cards. Also created new components for my projects sections, and added the respective gifs

--- src/components/atoms/IconComponent.jsx
+++ src/components/atoms/IconComponent.jsx
@@ -2,7 +2,7 @@
 import { Icon } from '@iconify-icon/react';
 
 const IconComponent = ({ icon }) => (
-  <Icon icon={icon} width="28" height="28" className="h-8 w-8"  aria-label={icon} />
+  <Icon icon={icon} width="32" height="32" className="h-8 w-8"  aria-label={icon} />
 );
 
 export default IconComponent;
