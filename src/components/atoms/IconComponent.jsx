b1bfcec2b3e77c460cdb3aeb7d144fad91467413
belfor-acuna <b.acuna.cas@gmail.com>
2025-02-07 02:55:31 -0300

    fix: :bug: Testing if there is a sintaxis error for the github page

--- src/components/atoms/IconComponent.jsx
+++ src/components/atoms/IconComponent.jsx
@@ -1,5 +1,6 @@
 // src/components/IconComponent.jsx
 import { Icon } from '@iconify-icon/react';
+import React from 'react';
 
 const IconComponent = ({ icon }) => (
   <Icon icon={icon} width="32" height="32" className="h-8 w-8"  aria-label={icon} />
